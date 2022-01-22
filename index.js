912d6bb5af0fecb78f7e5ab79f35ede6005396e8
Diego Barreto <diegobarreto5000@gmail.com>
2022-01-22 02:37:42 -0500

    Added SecondAnswer

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let firstAnswer  = window.prompt('You wake up from a large dream and you see a big castle in fornt of you... Go in or Run away?')
 if (firstAnswer == "Go in"){
     let secondAnswer = window.prompt("You have now 5 doors in fornt of you... But 3 of them need a key... One is a big red door, the other one is blue and tiny, so you are choosing big or small?")
+        if(secondAnswer == "Big"){
+            window.prompt("You have found a portal key, now you can go back home...")
+        }
 } else {
 
 }
